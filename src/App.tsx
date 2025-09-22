bfa0966323a572ec1bda42f6fc322866f0fb2d52
atovmasan <yt988998@gmail.com>
2025-09-22 15:59:34 +0300

    Day2

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const tracks = [
+    {
+      id: 1,
+      title: "Musicfun soundtrack",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+      id: 2,
+      title: "Musicfun soundtrack instrumental",
+      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+    {
+      id: 3,
+      title: "Musicfun soundtrack instrumental",
+      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+    {
+      id: 4,
+      title: "Musicfun soundtrack instrumental",
+      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+  ]
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Musicfun player</h1>
+      <ul>
+        {tracks.map((track) => (
+          <li key={track.id}>
+            <div>{track.title}</div>
+            <audio controls src={track.url}></audio>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
