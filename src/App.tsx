c201e0f070051b18c79fdba75e0997cd4805b9b3
atovmasan <yt988998@gmail.com>
2025-09-23 16:41:03 +0300

    Hook UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-// const tracks = []
-// const tracks = null
+import { useState } from "react"
+
+
+//const tracks = []
+//const tracks = null
 const tracks = [
   {
     id: 1,
@@ -12,19 +15,19 @@ const tracks = [
     url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
   },
 ]
-const selectedTrackId = 1
- 
+
 function App() {
+  const [selectedTrackId, setSelectedTrackId] = useState(null)
+ 
   if (tracks === null) {
-    return (
+     return (
       <div>
         <h1>Musicfun player</h1>
         <span>Loading...</span>
       </div>
     )
   }
- 
-  if (tracks.length === 0) {
+  if (tracks.length === 0) { 
     return (
       <div>
         <h1>Musicfun player</h1>
@@ -32,22 +35,23 @@ function App() {
       </div>
     )
   }
- 
+  
   return (
-    <>
-      <h1>Musicfun player</h1>
+    <div>
+      <h1>Musicfun</h1>
+      <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
       <ul>
         {tracks.map((track) => (
-          <li
-            key={track.id}
-            style={{ border: track.id === selectedTrackId ? "1px solid orange" : "none" }}
-          >
-            <div>{track.title}</div>
-            <audio controls src={track.url}></audio>
+          <li key={track.id} style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}>
+            <div onClick={() => {
+              setSelectedTrackId(track.id) // передаём реакту актуальный id
+            }}>{track.title}</div>
+            <audio src={track.url} controls={true}/>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
+
 export default App
