47dd869c2a6250cfe92c0470d1154779bbb45a94
atovmasan <yt988998@gmail.com>
2025-09-23 16:05:45 +0300

    3rd day

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// const tracks = []
+// const tracks = null
+const tracks = [
+  {
+    id: 1,
+    title: "Musicfun soundtrack",
+    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+  },
+  {
+    id: 2,
+    title: "Musicfun soundtrack instrumental",
+    url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+  },
+]
+const selectedTrackId = 1
+ 
 function App() {
-  const tracks = [
-    {
-      id: 1,
-      title: "Musicfun soundtrack",
-      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-    },
-    {
-      id: 2,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-    {
-      id: 3,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-    {
-      id: 4,
-      title: "Musicfun soundtrack instrumental",
-      url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-  ]
+  if (tracks === null) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+ 
+  if (tracks.length === 0) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>No tracks</span>
+      </div>
+    )
+  }
  
   return (
     <>
       <h1>Musicfun player</h1>
       <ul>
         {tracks.map((track) => (
-          <li key={track.id}>
+          <li
+            key={track.id}
+            style={{ border: track.id === selectedTrackId ? "1px solid orange" : "none" }}
+          >
             <div>{track.title}</div>
             <audio controls src={track.url}></audio>
           </li>
@@ -41,5 +50,4 @@ function App() {
     </>
   )
 }
-
 export default App
