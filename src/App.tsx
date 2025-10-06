ecb76f0151d2a0fe1ea219b9b12b22cba52e96bb
atovmasan <yt988998@gmail.com>
2025-10-06 14:28:49 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [selectedTrackId, setSelectedTrackId] = useState(null)
+  const [selectedTrack, setSelectedTrack] = useState(null)
   const [tracks, setTracks] = useState(null)
  
   useEffect(() => {
@@ -52,16 +53,38 @@ function App() {
     <div>
       <h1>Musicfun</h1>
       <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
+      <div style={{
+      display: "flex",
+      gap: "30px"
+    }}>
       <ul>
         {tracks.map((track) => (
           <li key={track.id} style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}>
             <div onClick={() => {
-              setSelectedTrackId(track.id) // передаём реакту актуальный id
+              setSelectedTrackId(track.id)
+              setSelectedTrack('loading')
+
+               fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + track.id, {
+      headers: {
+        'api-key': '3163379c-5c41-4b1b-9b49-59f416c1a346'
+      }
+    }).then(res => res.json()).then(json => setSelectedTrack(json.data))
             }}>{track.attributes.title}</div>
             <audio src={track.attributes.attachments[0].url} controls={true}/>
           </li>
         ))}
       </ul>
+      <div>
+        <h3>Details</h3>
+        {selectedTrack === null ? 'Track is not selectd':
+        selectedTrack === "loading" ? 'Loading':
+        <div>
+        <h4>{selectedTrack.attributes.title}</h4>
+        <p>{selectedTrack.attributes.lyrics ?? 'no lyrics'}</p>
+        </div>
+        }
+      </div>
+    </div>
     </div>
   )
 }
